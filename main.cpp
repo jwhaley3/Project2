4e4ac0e9d449ca744dd35282a1aa616f6782cd50
Justin <jwhaley3@asu.edu>
2021-02-27 18:03:46 -0700

    Finished Categories, Need to understand the dynamic memory allocation for BST

--- main.cpp
+++ main.cpp
@@ -1,6 +1,72 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include "defn.h"
+#include <cstring>
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    string queryInput;
+    cin >> queryInput;
+    int numCategories = stoi(queryInput);
+    string catName;
+    auto *app_categories = (struct categories *)malloc(numCategories * sizeof(struct categories));
+    if (app_categories == nullptr)
+    {
+        fprintf(stderr, "malloc failed \n");
+    }
+
+    //Read in all of the categories
+    for (int i = 0; i < numCategories; i++) {
+        getline(cin, catName);
+        strcpy_s(app_categories[i].category,catName.c_str());
+        app_categories[i].root = nullptr;
+
+    }//end of reading in all categories.
+
+    //Create the variables for running the loop
+    cin >> queryInput;
+    int numApps = stoi(queryInput);
+
+    //Create the variables for creating the BST
+
+    string name, category, version, units, tempSize, tempPrice;
+    float size, price;
+    int categoryIndex;
+
+    for (int i = 0; i < numApps; i++)
+    {
+        //Assign all values to strings that will be later imported into struct app_info
+        getline(cin, category);
+        getline(cin, name);
+        getline(cin, version);
+        getline(cin, tempSize);
+        getline(cin, units);
+        getline(cin, tempPrice);
+
+
+
+
+
+
+
+
+
+        //Find which BST this should be apart of
+        for (int j = 0; j < numCategories; j++)
+        {
+            if (category == app_categories[j].category) {
+                categoryIndex = j;
+                break; //Don't run through everything once we get a match
+            }
+        }
+        //Now we have the category index for app_categories, we can traverse the tree
+
+
+    }
+
+
+
 }
