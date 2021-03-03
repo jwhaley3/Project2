0353233b6f96865cbff629fe9a7a2709af1e5a5d
Justin <jwhaley3@asu.edu>
2021-03-03 15:50:37 -0700

    Created Nodes with dynamic Memory allocation, currently don't understnad bst.cc, as it's not a class but can I make it a class?

---
+++ bst.cpp
@@ -0,0 +1,10 @@
+#include "defn.h"
+
+//
+// Created by Justin on 3/3/2021.
+//
+//Function to add the node to the binary search tree (BST)
+int addNode(app_info * node)
+{
+
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstring>
 #include "defn.h"
+#include "bst.cpp"
 #include <cstring>
 using namespace std;
 
@@ -17,17 +18,19 @@ int main() {
     {
         fprintf(stderr, "malloc failed \n");
     }
-
+    cin.ignore(100,'\n');
     //Read in all of the categories
     for (int i = 0; i < numCategories; i++) {
         getline(cin, catName);
-        strcpy_s(app_categories[i].category,catName.c_str());
+        strcpy(app_categories[i].category,catName.c_str());
         app_categories[i].root = nullptr;
 
     }//end of reading in all categories.
 
     //Create the variables for running the loop
+
     cin >> queryInput;
+
     int numApps = stoi(queryInput);
 
     //Create the variables for creating the BST
@@ -36,8 +39,14 @@ int main() {
     float size, price;
     int categoryIndex;
 
+    ///////////////////////////////////////////////////////////////////////
+    cin.ignore(100,'\n');
+    //Loop to create the nodes for the apps
     for (int i = 0; i < numApps; i++)
     {
+        //Create the node using malloc
+        auto * newNode = (struct app_info*)malloc(numCategories * sizeof(struct app_info));
+
         //Assign all values to strings that will be later imported into struct app_info
         getline(cin, category);
         getline(cin, name);
@@ -46,13 +55,17 @@ int main() {
         getline(cin, units);
         getline(cin, tempPrice);
 
+        //Convert strings to integers that need to change
+        size = stof(tempSize);
+        price = stof(tempPrice);
 
-
-
-
-
-
-
+        //Assign all values into the newNode;
+        strcpy(newNode->category, category.c_str());
+        strcpy(newNode->app_name, name.c_str());
+        strcpy(newNode->version, version.c_str());
+        newNode->size = size;
+        strcpy(newNode->units, units.c_str());
+        newNode->price = price;
 
         //Find which BST this should be apart of
         for (int j = 0; j < numCategories; j++)
@@ -61,12 +74,15 @@ int main() {
                 categoryIndex = j;
                 break; //Don't run through everything once we get a match
             }
-        }
+        }//End of search loop
         //Now we have the category index for app_categories, we can traverse the tree
 
 
-    }
+
+    }//End of numApps for loop
 
 
 
-}
+    free(app_categories);
+
+}//End of main
